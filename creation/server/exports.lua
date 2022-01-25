9b32d319c1ef6c1a1cb321b7c88b9228196b3931
Paul Reed <45607581+TheCoolestPaul@users.noreply.github.com>
2022-01-25 14:00:54 -0600

    Added boxZone

--- creation/server/exports.lua
+++ creation/server/exports.lua
@@ -16,5 +16,14 @@ exports("createPolyZone", function(polyName, polyType, polyData, debugPolyIn)
             name = polyName,
             debugPoly = debugPolyIn,
         })
+    elseif ( string.lower( polyType ) == "box" ) then
+        if (not polyData.scale or not polyData.scale.x or not polyData.scale.y or not polyData.scale.z or not polyData.center or not polyData.center.x or not polyData.center.y or not polyData.center.z or not polyData.width or not polyData.length ) then
+            return print(("You didn't pass createPolyZone the correct polyData! Data: [%s]"):format(json.encode(polyData)))
+        end
+        polyZones[polyName] = BoxZone:Create(vector3(polyData.center.x, polyData.center.y, polyData.center.z), polyData.length, polyData.width, {
+            name = polyName,
+            scale = {polyData.scale.x, polyData.scale.y, polyData.scale.z},
+            debugPoly = debugPolyIn,
+        })
     end
 end)
